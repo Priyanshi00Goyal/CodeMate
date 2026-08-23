2ea132da078d8bb6aa9e82901977db8c0f59debe
Priyanshi Goyal <p.25012008.g@gmail.com>
2026-08-23 18:12:43 +0530

    Implement syntax checking for Python code
    Add a function to check Python syntax using ast module.

--- modules/code_analyzer.py
+++ modules/code_analyzer.py
@@ -1,1 +1,23 @@
+import ast
 
+
+def check_python_syntax(code):
+
+    try:
+        ast.parse(code)
+
+        return {
+            "valid": True,
+            "message": "No syntax errors detected.",
+            "line": None,
+            "offset": None
+        }
+
+    except SyntaxError as error:
+
+        return {
+            "valid": False,
+            "message": error.msg,
+            "line": error.lineno,
+            "offset": error.offset
+        }
